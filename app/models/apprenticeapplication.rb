534d84890872130225f09ae97c4c90884f2a1a27
Div <divya@heysparkbox.com>
2014-06-03 14:43:31 -0400

    Added validation tests and fixtures

--- app/models/apprenticeapplication.rb
+++ app/models/apprenticeapplication.rb
@@ -1,2 +1,3 @@
 class Apprenticeapplication < ActiveRecord::Base
+  validates :name, presence: true
 end
--- test/models/apprenticeapplication_test.rb
+++ test/models/apprenticeapplication_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ApprenticeapplicationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @a = apprenticeapplications(:bob)
+  end
+
+  test 'valid only with all attributes present' do
+    assert @a.valid?, 'Apprentice application was not valid'
+  end
+
+  test 'it should be invalid with an empty name' do
+    @a.name = ''
+    assert !@a.valid?, 'invalid without a name'
+  end
 end

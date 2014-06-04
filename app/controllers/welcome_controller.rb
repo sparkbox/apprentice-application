07b6ab33890f8cd15bde148580bcd30bd4b680dc
Div <divya@heysparkbox.com>
2014-06-04 10:02:43 -0400

    Edited routes.rb file so form on apply page renders

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @apprenticeapplication = Apprenticeapplication.new
   end
 end
---
+++ app/models/apprenticeapplication.rb
@@ -0,0 +1,3 @@
+class Apprenticeapplication < ActiveRecord::Base
+  attr_accessor :name, :email
+end

c7325d4889a20e6871eda139b3eb78dfee4ff5cd
Div <divya@heysparkbox.com>
2014-06-06 15:28:19 -0400

    Created class associated with form builder

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,18 @@ class WelcomeController < ApplicationController
   def index
     @apprenticeform = ApprenticeForm.new
   end
+
+  def create
+    @apprenticeform = ApprenticeForm.new(apprentice_params)
+
+    respond_to do |format|
+      if @apprenticeform.save
+        format.html { redirect_to @apprenticeform, notice: 'Apprentice was successfully created.' }
+        format.json { render :show, status: :created, location: @apprenticeform }
+      else
+        format.html { render :new }
+        format.json { render json: @apprenticeform.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end

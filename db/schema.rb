0bcb4056a8b8253dca505b9326569fc116cddf1b
michaelprflores <michaelprflores@gmail.com>
2014-06-09 11:14:13 -0400

    fix (db): remove original "apprenticeapplications" table
    This table started from move Apprenticeapplication.
    Was then modified to ApprenticeApplication, finally
    a new table was created called apprentice_forms.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140603143943) do
+ActiveRecord::Schema.define(version: 20140609150705) do
 
   create_table "apprentice_forms", force: true do |t|
     t.string   "name"

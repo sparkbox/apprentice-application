2101a390bb694017828457214941db7b2d972b55
Div <divya@heysparkbox.com>
2014-06-16 13:05:29 -0400

    Removed/Commented out model validation so that form moves through the different steps of the wizard. Need to figure out how to save an object and have it remember/save fields from previous steps.

--- app/controllers/demographics_controller.rb
+++ app/controllers/demographics_controller.rb
@@ -6,24 +6,21 @@ class DemographicsController < ApplicationController
 
   def create
       # return params
-      @apprenticeform = ApprenticeForm.new
-  #   if @apprenticeform.save
-  #     flash[:notice] = 'Apprentice was successfully created'
-  #     redirect_to welcome_path(@apprenticeform)
-  #   end
-  # end
-
-    respond_to do |format|
+      @apprenticeform = ApprenticeForm.new(apprentice_form_params)
       if @apprenticeform.save
         flash[:notice] = 'Apprentice was successfully created.'
-        format.html { redirect_to step_three_path}
-        format.json { render :show, status: :created, location: @apprenticeform }
+        redirect_to step_three_path
       else
         flash[:notice] = 'Empty fields not allowed.'
-        format.html { render :index }
-        format.json { render json: @apprenticeform.errors, status: :unprocessable_entity }
+        render :index
       end
+
     end
+
+    private
+    def apprentice_form_params
+      params.require(:apprentice_form).permit(:first_name, :last_name, :email, :start_date, :end_date, :willing_to_move)
+    end
+
   end
 
-end
--- app/models/apprentice_form.rb
+++ app/models/apprentice_form.rb
@@ -1,16 +1,6 @@
 class ApprenticeForm < ActiveRecord::Base
 
-  attr_writer :current_step
-
-  validates :first_name, presence: true, length: {maximum: 50}
-  validates :last_name, presence: true
-
-  def current_step
-    @current_step || steps.first
-  end
-
-  def steps
-    %w[basic_information demographic personality confirmation]
-  end
+  # validates :first_name, presence: true, length: {maximum: 50}
+  # validates :last_name, presence: true
 
 end
